d34803347dd61b0ddc606e13864a55540a832306
MinhTrieu <trieunguyen.31241027455@st.ueh.edu.vn>
2026-06-03 09:44:26 +0700

    Khởi tạo bộ khung kiến trúc 3 lớp

---
+++ BUS/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace BUS;
+
+public class Class1
+{
+}
---
+++ GUI/Form1.cs
@@ -0,0 +1,9 @@
+namespace Desktop_Application_Development;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
